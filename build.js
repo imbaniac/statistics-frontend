c9d2b3159cb8c3155bf3dfe5584f462c34c65ffe
imbaniac <tima666tima@gmail.com>
2016-08-29 15:11:05 +0300

    Skeleton setup

---
+++ build.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-console */
+
+import webpack from 'webpack';
+import webpackConfig from './webpack.config.prod';
+import colors from 'colors';
+
+process.env.NODE_ENV = 'production';
+console.log('Minificating...');
+
+webpack(webpackConfig).run((err, stats) => {
+	if(err){
+		console.log(err.bold.red);
+		return 1;
+	}
+	const jsonStats = stats.toJson();
+	if(jsonStats.hasErrors){
+		return jsonStats.errors.map(error => {
+			console.log(error.red);
+		});
+	}
+	if(jsonStats.hasWarnings){
+		console.log('Webpack have the following warnings:'.bold.yellow);
+		jsonStats.warnings.map(w => {
+			console.log(w.yellow);
+		});
+	}
+
+	console.log(`Webpack stats ${stats}`);
+	console.log('Ready'.green);
+	return 0;
+});
